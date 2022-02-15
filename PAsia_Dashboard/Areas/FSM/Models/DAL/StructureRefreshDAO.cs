94ca68acee6da7d84cdd32de4cdd44634c648404
Md. Arif Hossain <arif.hossain177@gmail.com>
2022-02-15 15:39:19 +0600

    FSM_structure_Refresh_15022022

---
+++ PAsia_Dashboard/Areas/FSM/Models/DAL/StructureRefreshDAO.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.OracleClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using PAsia_Dashboard.Areas.FSM.Models.BEL;
+using PAsia_Dashboard.Areas.FSM.Models.BEL.BEO;
+using PAsia_Dashboard.Universal.Gateway;
+
+namespace PAsia_Dashboard.Areas.FSM.Models.DAL
+{
+    public class StructureRefreshDAO:ReturnData
+    {
+        private readonly DBConnection dbConn = new DBConnection();
+        private readonly DBHelper dbHelper = new DBHelper();
+
+        public object GetStructureRefreshValue(string fromDate, string toDate)
+        {
+            try
+            {
+
+                //fromDate =( fromDate == "" || fromDate == null) ? "01/02/2022" : fromDate;
+                //toDate = toDate == "" || toDate ==null ? "14/02/2022" : toDate;
+
+                using (OracleConnection objConn = new OracleConnection(dbConn.SAConnStrReader("Sales")))
+                {
+                    using (OracleCommand objCmd = new OracleCommand())
+                    {
+                        objCmd.Connection = objConn;
+                        objCmd.CommandText = "PRC_PAL_FIELD_FORCE_MIO";
+                        objCmd.CommandType = CommandType.StoredProcedure;                                             
+                        objConn.Open();
+                        objCmd.ExecuteNonQuery();
+                       
+                        return 0;
+
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return ExceptionReturn = "";
+            }
+        }
+
+    }
+}
---
+++ PAsia_Dashboard/obj/Release/Package/PackageTmp/Areas/FSM/Controllers/StructureRefreshController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PAsia_Dashboard.Areas.FSM.Models.BEL.BEO;
+using PAsia_Dashboard.Areas.FSM.Models.DAL;
+using PAsia_Dashboard.Areas.FSM.Models.DAL.DAO;
+using PAsia_Dashboard.Universal.Gateway;
+
+namespace PAsia_Dashboard.Areas.FSM.Controllers
+{
+    [LogInChecker]
+    public class StructureRefreshController : Controller
+    {
+
+        StructureRefreshDAO StructureRefreshDAO = new StructureRefreshDAO();
+        // GET: FSM/StructureRefresh
+        public ActionResult frmStructureRefresh()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetStructureRefreshValue(string fromDate, string toDate)
+        {
+            var listData = StructureRefreshDAO.GetStructureRefreshValue(fromDate, toDate);
+            var data = Json(listData, JsonRequestBehavior.AllowGet);
+            data.MaxJsonLength = int.MaxValue;
+            return data;
+
+        }
+
+    }
+}
